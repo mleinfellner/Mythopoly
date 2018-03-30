06f6071eb0dc1474c14394045ff90d0e52d4ac04
Max Leinfellner <max@leinfellner.com>
2018-03-30 14:33:59 +0100

    Added raining message

--- src/monopoly/Monopoly.java
+++ src/monopoly/Monopoly.java
@@ -17,6 +17,7 @@ public class Monopoly {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        System.out.println("Its Raining");
     }
     
 }
