d62bd21b71bef8a0a1edd660f8457f53fee64985
Richard Leinfellner <richard@leinfellner.com>
2018-03-30 12:23:03 +0100

    Fixed text typo

--- src/monopoly/Monopoly.java
+++ src/monopoly/Monopoly.java
@@ -16,7 +16,7 @@ public class Monopoly {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hellow World");
+        System.out.println("Hello World");
     }
     
 }
